2f9eace2b0d7e679b83ce3bd9ce6a48bd1f9f21b
Jeevan James <jeevanjj@live.com>
2020-09-15 18:58:36 +0530

    Assorted changes
    - Add console and console tool templates.
    - Cleaned up template jsons.

---
+++ templates/Jeevan.Console.Tool/Program.cs
@@ -0,0 +1,11 @@
+//TODO: Correct namespace
+namespace SymbolNamespace
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Code here
+        }
+    }
+}
