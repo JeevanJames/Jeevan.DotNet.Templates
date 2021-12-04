403b52e88f08241667ac95df79b2c0c1d10e7188
Jeevan James <jeevanjj@live.com>
2021-12-04 12:57:21 +0530

    Update templates

--- templates/Jeevan.Console.Tool/Program.cs
+++ templates/Jeevan.Console.Tool/Program.cs
@@ -1,11 +1,1 @@
-//TODO: Correct namespace
-namespace SymbolNamespace
-{
-    internal static class Program
-    {
-        private static void Main(string[] args)
-        {
-            // Code here
-        }
-    }
-}
+// Start code here
